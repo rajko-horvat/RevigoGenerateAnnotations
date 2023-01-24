e686a798c242575f81aa12b3e2ab28511992204c
rajko-horvat <rajko.horvat@irb.hr>
2023-01-24 11:01:21 +0100

    Update paths

--- Program.cs
+++ Program.cs
@@ -5,6 +5,32 @@ using IRB.Revigo.Databases;
 
 namespace GenerateSpeciesAnnotations
 {
+	/// <summary>
+	/// Utility that generates Species Annotations for the REVIGO
+	/// 
+	/// Authors:
+	/// 	Rajko Horvat (rhorvat at irb.hr)
+	/// 
+	/// License:
+	/// 	MIT
+	/// 	Copyright (c) 2011-2023, Ruđer Bošković Institute
+	///		
+	/// 	Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+	/// 	and associated documentation files (the "Software"), to deal in the Software without restriction, 
+	/// 	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+	/// 	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+	/// 	subject to the following conditions: 
+	/// 	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	/// 	The names of authors and contributors may not be used to endorse or promote Software products derived from this software 
+	/// 	without specific prior written permission.
+	/// 	
+	/// 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+	/// 	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+	/// 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+	/// 	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+	/// 	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+	/// 	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	/// </summary>
 	class Program
 	{
 		static void Main(string[] args)
@@ -46,13 +72,13 @@ namespace GenerateSpeciesAnnotations
 			};
 
 			SpeciesAnnotationsList annotations = SpeciesAnnotationsList.FromGOA(
-				@"c:\Users\rajko\Documents\Projects\Revigo\Databases\EBI_GOA\2022_09_16\goa_uniprot_gcrp.gaf",
-				@"c:\Users\rajko\Documents\Projects\Revigo\Databases\GeneOntology\2022_11_03\go.obo",
-				@"c:\Users\rajko\Documents\Projects\Revigo\Databases\Taxonomy_NCBI\2022_11_18\names.dmp",
+				@"c:\Revigo\Databases\EBI_GOA\2022_09_16\goa_uniprot_gcrp.gaf",
+				@"c:\Revigo\Databases\GeneOntology\2022_11_03\go.obo",
+				@"c:\Revigo\Databases\Taxonomy_NCBI\2022_11_18\names.dmp",
 				aTaxonIDs);
 
 			// serialize GOTermSizes object
-			annotations.Serialize(@"c:\Users\rajko\Documents\Projects\Revigo\Databases\SpeciesAnnotations.xml", false);
+			annotations.Serialize(@"c:\Revigo\Databases\SpeciesAnnotations.xml", false);
 
 			Console.WriteLine("Finished, press Enter key");
 			Console.ReadLine();
